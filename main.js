9c294cc7695fd39e1142095ed97df6e2a86e76e7
hatake <hugoro3000@gmail.com>
2022-08-19 00:44:32 -0500

    implemente menú carrito de compras

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
+/*desktop menu*/
 const menuEmail = document.querySelector(".navbar-email");
-const desktopMenu = document.querySelector(".desktop-menu");
-
+const menuDesktop = document.querySelector(".desktop-menu");
 menuEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu() {
-    desktopMenu.classList.toggle('inactive');
+    //CERRAR CARRITO
+    const ismenuCarritoClosed = menuCarrito.classList.contains('inactive');
+    
+    if(!ismenuCarritoClosed){
+    menuCarrito.classList.add('inactive');
+    }
+
+    menuDesktop.classList.toggle('inactive');
+
+}
+/*CARRITO*/
+const carritoIcon = document.querySelector(".navbar-shopping-cart");
+const menuCarrito =document.querySelector(".product-detail");
+carritoIcon.addEventListener('click', toggleCarritoAside);
+
+function toggleCarritoAside() {
+    //CERRAR MENU MOBILE
+    const ismenuMobClosed = menuMob.classList.contains('inactive');
+
+    if(!ismenuMobClosed){
+    menuMob.classList.add('inactive');
+    }
+
+    menuCarrito.classList.toggle('inactive');
 }
 
+/*Menu mobile*/
 const menuHam = document.querySelector(".menu-hamburguesa");
 const menuMob = document.querySelector(".mobile-menu");
-
 menuHam.addEventListener('click', toggleMenumob);
 
 function toggleMenumob() {
+    //CERRAR CARRITO
+    const ismenuCarritoClosed = menuCarrito.classList.contains('inactive');
+    
+    if(!ismenuCarritoClosed){
+    menuCarrito.classList.add('inactive');
+    }
+
     menuMob.classList.toggle('inactive');
 }
+
+
